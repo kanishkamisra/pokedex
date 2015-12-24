239ff6f5141045189b716b7d552d4c45be8de11d
vincentvangoat <menogetusername@gmail.com>
2015-12-23 22:53:38 -0600

    Stored scraped data to a json file

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 import lxml.html as lh
+import json
 
 url = 'http://pokemondb.net/pokedex/all'
 page = requests.get(url)
@@ -15,7 +16,6 @@ i=0
 for e in elements[0]:
 	i+=1
 	name = e.text_content()
-	# print '%d: %s' % (i,name)
 	col.append((name,[]))
 
 # Let's populate!
@@ -28,7 +28,6 @@ for j in range(1,len(elements)):
 	i=0
 	for e in E.iterchildren():
 		data = e.text_content()
-
 		if i>0:
 			try:
 				data = int(data)
@@ -37,7 +36,13 @@ for j in range(1,len(elements)):
 		col[i][1].append(data)
 		i+=1
 
-# print [len(E) for (title,E) in col]
+data_list = []
+# Populate a separate list with json formatted data.
+for i in range(0,len(elements)-1):
+	data_list.append({column: entry[i] for (column, entry) in col})
 
-data_dict = { column:entry for (column, entry) in col }
+# Dump to json
+with open('pokemondata.json', 'wb') as jsonfile:
+	json.dump(data_list, jsonfile)
+
 
